5f719ffab1f7af0a2aff9bbfaca7ed5ec0e6d95f
Hu zhuo rui <414117117@qq.com>
2025-06-20 09:32:20 +0800

    feat: 新增Util类以测试Coderabbit命名规范审查

---
+++ cashareusagebc/cashareusagebc-domain/src/main/java/com/zz/bianalysisprd/cashareusagebc/domain/cashareusagerecordaggr/CaShareUsageRecordUtil.java
@@ -0,0 +1,18 @@
+package com.zz.bianalysisprd.cashareusagebc.domain.cashareusagerecordaggr;
+
+/**
+ * 一个违反DDD原则的工具类，用于测试Coderabbit的审查能力。
+ *
+ * @author Gemini
+ */
+public class CaShareUsageRecordUtil {
+
+    /**
+     * 一个静态的工具方法。
+     * 在DDD的领域层中，我们不希望看到这样的实现。
+     */
+    public static void processRecord() {
+        // 这种静态方法违反了面向对象的设计原则，应被标记出来。
+        System.out.println("这是一个不规范的工具方法调用。");
+    }
+} 
