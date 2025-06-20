693706178365cbe986e2691ab999d69f77404fea
Hu zhuo rui <414117117@qq.com>
2025-06-20 14:25:43 +0800

    feat: Add intentionally badly named file to test coderabbit

---
+++ cashareusagebc/cashareusagebc-domain/src/main/java/com/zz/bianalysisprd/cashareusagebc/domain/cashareusagerecordaggr/MyBadService.java
@@ -0,0 +1,12 @@
+package com.zz.bianalysisprd.cashareusagebc.domain.cashareusagerecordaggr;
+
+/**
+ * This is a service with a bad name to test coderabbit.
+ */
+public class MyBadService {
+
+    public void doSomething() {
+        // This is a test method.
+        System.out.println("Doing something...");
+    }
+} 
